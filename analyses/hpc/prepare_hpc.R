c2f7c72b82b7dd9cb3859496a82655d2ce7e0ae5
simonpcouch <simonpatrickcouch@gmail.com>
2024-05-02 16:18:54 -0500

    remove candidates that aren't able to `predict()`

--- analyses/hpc/prepare_hpc.R
+++ analyses/hpc/prepare_hpc.R
@@ -44,39 +44,39 @@ norm_recipe <-
   step_zv(all_predictors()) %>%
   step_normalize(all_numeric_predictors())
 
-glmnet_recipe <- 
+glmnet_recipe <-
   recipe(formula = class ~ ., data = train) %>%
-  step_log(compounds, input_fields) %>% 
-  step_log(num_pending, offset = 1) %>% 
-  step_dummy(all_nominal_predictors()) %>% 
-  step_zv(all_predictors()) %>% 
-  step_normalize(all_numeric_predictors()) %>% 
-  step_ns(compounds, deg_free = tune("compounds")) %>% 
-  step_ns(input_fields, deg_free = tune("inputs")) 
+  step_log(compounds, input_fields) %>%
+  step_log(num_pending, offset = 1) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_zv(all_predictors()) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  step_ns(compounds, deg_free = tune("compounds")) %>%
+  step_ns(input_fields, deg_free = tune("inputs"))
 
 # ------------------------------------------------------------------------------
 
-glmnet_spec <- 
-  multinom_reg(penalty = tune(), mixture = tune()) %>% 
-  set_mode("classification") %>% 
-  set_engine("glmnet") 
+glmnet_spec <-
+  multinom_reg(penalty = tune(), mixture = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("glmnet")
 
-glmnet_workflow <- 
-  workflow() %>% 
-  add_recipe(glmnet_recipe) %>% 
-  add_model(glmnet_spec) 
+glmnet_workflow <-
+  workflow() %>%
+  add_recipe(glmnet_recipe) %>%
+  add_model(glmnet_spec)
 
-glmnet_param <- 
-  glmnet_workflow %>% 
-  extract_parameter_set_dials() %>% 
+glmnet_param <-
+  glmnet_workflow %>%
+  extract_parameter_set_dials() %>%
   update(
     compounds    = spline_degree(c(2L, 50L)),
     inputs = spline_degree(c(2L, 50L))
   )
 
 set.seed(23892)
-glmnet_grid <- 
-  glmnet_param %>% 
+glmnet_grid <-
+  glmnet_param %>%
   grid_max_entropy(size = 25)
 
 set.seed(9624)
@@ -87,7 +87,7 @@ glmnet_res <-
     grid = glmnet_grid,
     param_info = glmnet_param,
     control = ctrl_grid
-  ) 
+  )
 
 save(
   glmnet_res,
@@ -98,15 +98,15 @@ save(
 
 # ------------------------------------------------------------------------------
 
-cart_bag_spec <- 
+cart_bag_spec <-
   bag_tree() %>%
-  set_engine("rpart", times = 50) %>% 
+  set_engine("rpart", times = 50) %>%
   set_mode("classification")
 
-cart_bag_workflow <- 
-  workflow() %>% 
-  add_recipe(bland_recipe) %>% 
-  add_model(cart_bag_spec) 
+cart_bag_workflow <-
+  workflow() %>%
+  add_recipe(bland_recipe) %>%
+  add_model(cart_bag_spec)
 
 set.seed(9624)
 cart_bag_res <-
@@ -114,7 +114,7 @@ cart_bag_res <-
     cart_bag_workflow,
     resamples = hpc_rs,
     control = ctrl_grid
-  ) 
+  )
 
 save(
   cart_bag_res,
@@ -125,18 +125,18 @@ save(
 
 # ------------------------------------------------------------------------------
 
-fda_recipe <- 
+fda_recipe <-
   recipe(formula = class ~ ., data = hpc_data) %>%
-  step_log(compounds, input_fields) %>% 
+  step_log(compounds, input_fields) %>%
   step_log(num_pending, offset = 1)
 
-fda_spec <- 
+fda_spec <-
   discrim_flexible(num_terms = tune(), prod_degree = tune(), prune_method = "none")
 
-fda_workflow <- 
-  workflow() %>% 
-  add_recipe(fda_recipe) %>% 
-  add_model(fda_spec) 
+fda_workflow <-
+  workflow() %>%
+  add_recipe(fda_recipe) %>%
+  add_model(fda_spec)
 
 fda_grid <- crossing(prod_degree = 1:2, num_terms = 2:40)
 
@@ -147,7 +147,7 @@ fda_res <-
     resamples = hpc_rs,
     grid = fda_grid,
     control = ctrl_grid
-  ) 
+  )
 
 save(
   fda_res,
@@ -158,58 +158,18 @@ save(
 
 # ------------------------------------------------------------------------------
 
-nnet_spec <-
-  mlp(
-    hidden_units = tune(),
-    penalty = tune(),
-    epochs = tune()
-  ) %>%
-  set_engine("nnet", MaxNWts = 5000) %>%
+svmp_spec <-
+  svm_poly(cost = tune(), degree = tune(), scale_factor = tune()) %>%
   set_mode("classification")
 
-nnet_workflow <-
+svmp_workflow <-
   workflow() %>%
   add_recipe(norm_recipe) %>%
-  add_model(nnet_spec)
+  add_model(svmp_spec)
 
-nnet_param <-
-  nnet_workflow %>%
-  extract_parameter_set_dials() %>%
-  update(
-    hidden_units = hidden_units(c(2, 75))
-  )
-
-set.seed(9624)
-nnet_res <-
-  tune_grid(
-    nnet_workflow,
-    resamples = hpc_rs,
-    grid = 25,
-    param_info = nnet_param,
-    control = ctrl_grid
-  )
-
-save(
-  nnet_res,
-  file = file.path(fits_dir, "hpc_nnet.RData"),
-  compress = "xz",
-  compression_level = 9
-)
-
-# ------------------------------------------------------------------------------
-
-svmp_spec <- 
-  svm_poly(cost = tune(), degree = tune(), scale_factor = tune()) %>% 
-  set_mode("classification") 
-
-svmp_workflow <- 
-  workflow() %>% 
-  add_recipe(norm_recipe) %>% 
-  add_model(svmp_spec) 
-
-svmp_param <- 
-  svmp_workflow %>% 
-  extract_parameter_set_dials() 
+svmp_param <-
+  svmp_workflow %>%
+  extract_parameter_set_dials()
 
 set.seed(9624)
 svmp_res <-
@@ -219,7 +179,7 @@ svmp_res <-
     grid = 25,
     param_info = svmp_param,
     control = ctrl_grid
-  ) 
+  )
 
 save(
   svmp_res,
@@ -230,21 +190,21 @@ save(
 
 # ------------------------------------------------------------------------------
 
-xgb_recipe <- 
-  recipe(formula = class ~ ., data = hpc_data) %>% 
-  step_dummy(all_nominal_predictors(), one_hot = TRUE) %>% 
-  step_zv(all_predictors()) 
+xgb_recipe <-
+  recipe(formula = class ~ ., data = hpc_data) %>%
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) %>%
+  step_zv(all_predictors())
 
-xgb_spec <- 
-  boost_tree(trees = tune(), min_n = tune(), tree_depth = tune(), learn_rate = tune(), 
-             loss_reduction = tune(), sample_size = tune()) %>% 
-  set_mode("classification") %>% 
-  set_engine("xgboost") 
+xgb_spec <-
+  boost_tree(trees = tune(), min_n = tune(), tree_depth = tune(), learn_rate = tune(),
+             loss_reduction = tune(), sample_size = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("xgboost")
 
-xgb_workflow <- 
-  workflow() %>% 
-  add_recipe(xgb_recipe) %>% 
-  add_model(xgb_spec) 
+xgb_workflow <-
+  workflow() %>%
+  add_recipe(xgb_recipe) %>%
+  add_model(xgb_spec)
 
 set.seed(9624)
 xgb_res <-
@@ -253,7 +213,7 @@ xgb_res <-
     resamples = hpc_rs,
     grid = 25,
     control = ctrl_grid
-  ) 
+  )
 
 save(
   xgb_res,
@@ -266,8 +226,8 @@ save(
 
 save(
   train, test,
-  file = file.path("analyses", "hpc", "hpc_data.RData"), 
-  compress = "xz", 
+  file = file.path("analyses", "hpc", "hpc_data.RData"),
+  compress = "xz",
   compression_level = 9
 )
 
