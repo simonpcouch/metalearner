f79793938c5f5f58611aeb7859bffeb1c010f487
simonpcouch <simonpatrickcouch@gmail.com>
2024-05-06 15:40:12 -0500

    bump number of nnet weights

--- meta_learners/specs/make_spec_nn.R
+++ meta_learners/specs/make_spec_nn.R
@@ -1,5 +1,5 @@
 make_spec <- function() {
   mlp(hidden_units = tune(), penalty = tune(), epochs = tune()) %>%
-    set_engine("nnet") %>%
+    set_engine("nnet", MaxNWts = 1e5) %>%
     set_mode("regression")
 }
